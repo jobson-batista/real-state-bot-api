da1bbaf99c3c849d122c90558b3738f57299af19
Jobson Batista <jobson.silva@dcx.ufpb.br>
2022-04-06 09:11:36 -0300

    Added congiration heroku

--- src/app.py
+++ src/app.py
@@ -2,12 +2,15 @@ from flask import Flask
 from services import web_scraping_service as ws
 import json
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
-@app.route("/search/<string:city>/<string:district>", methods=['GET'])
+@APP.route("/search/<string:city>/<string:district>", methods=['GET'])
 def search_by_city_district(city, district):
     return json.loads(ws.search_last_ad(city, district))
 
-@app.route("/search", methods=['GET'])
+@APP.route("/search", methods=['GET'])
 def search():
     return json.loads(ws.search_last_ad())
+
+if __name__ == '__main__':
+    APP.run()
