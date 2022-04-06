da0d2b7812701ec2a2085bea9ca78c58ffdfda64
Jobson Batista <jobson.silva@dcx.ufpb.br>
2022-04-06 07:46:29 -0300

    Implemented city and district search endpoints

--- src/app.py
+++ src/app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from services import web_scraping_service as ws
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "It's Working!"
+@app.route("/search/<string:city>/<string:district>", methods=['GET'])
+def search_by_city_district(city, district):
+    return json.loads(ws.search_last_ad(city, district))
+
+@app.route("/search", methods=['GET'])
+def search():
+    return json.loads(ws.search_last_ad())
--- src/services/web_scraping_service.py
+++ src/services/web_scraping_service.py
@@ -1,12 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
-OLX_URL_BASE = 'https://pb.olx.com.br/paraiba/joao-pessoa/imoveis'
+OLX_URL_BASE = 'https://pb.olx.com.br/paraiba/'
+headers = {
+    'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
+}
 
-def search():
-    headers = {
-        'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
-    }
-    r = requests.get(OLX_URL_BASE, headers=headers)
+def search_last_ad(city='',district=''):
+    URL = OLX_URL_BASE
+    last_ad = {}
+    if(city != '' and district != ''):
+        URL = URL+city+'/'+district+'/imoveis'
+    else:
+        URL = URL+'imoveis'
+    try:
+        r = requests.get(URL, headers=headers)
+        print("\nURL Request: "+r.url+"\n")
+    except requests.ConnectionError as e:
+        print("Erro ao fazer requisição!\n",e)
     soup = BeautifulSoup(r.content, 'html.parser')
-    print(soup.title)
+    data_general = soup.find(id='initial-data').get('data-json')
+    ad_list = json.loads(data_general)['listingProps']['adList']
+
+    last_ad["title"] = ad_list[0]["subject"]
+    last_ad["date"] = ad_list[0]["date"]
+    last_ad["location"] = ad_list[0]["location"]
+    last_ad["url"] = ad_list[0]["url"]
+    last_ad["image"] = ad_list[0]["thumbnail"]
+    last_ad["price"] = ad_list[0]["price"].split(" ")[1]
+
+    return json.dumps(last_ad)
