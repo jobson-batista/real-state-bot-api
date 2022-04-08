3585fbd914189fd8a665361fa858cdc14e5b47a0
Jobson Batista <jobson.silva@dcx.ufpb.br>
2022-04-08 20:39:27 -0300

    Added multithread

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+from concurrent.futures import thread
 from flask import Flask, jsonify
 import services.web_scraping as ws
 import json
 
 app = Flask(__name__)
+app.run(threaded = True)
 
 @app.route("/search/<string:city>/<string:district>", methods=['GET'])
 def search_by_city_district(city, district):
