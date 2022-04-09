dedd3fa0fd896656d73362fcc5934e96f5464270
Jobson Batista <jobson.silva@dcx.ufpb.br>
2022-04-09 00:21:19 -0300

    Added params variables in the search

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from concurrent.futures import thread
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import services.web_scraping as ws
 import json
 
 app = Flask(__name__)
 
-@app.route("/search/<string:city>/<string:district>", methods=['GET'])
-def search_by_city_district(city, district):
-    return json.loads(ws.search_last_ad(city, district))
+@app.route("/search", methods=['GET'])
+def search_by_city_district():
+    res = []
+    city = request.args.get('city')
+    districts_list = request.args.getlist('district')
+    for district in districts_list:
+        ad = ws.search_last_ad(city, district)
+        res.append(json.loads(ad))
+    return jsonify(res)        
 
 @app.route("/search", methods=['GET'])
 def search():
