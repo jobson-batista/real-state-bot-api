378e19f72a4d0edc1829ca2e4b83840b53a15fc8
Jobson Batista <jobson.silva@dcx.ufpb.br>
2022-04-09 00:31:02 -0300

    Removed function

--- app.py
+++ app.py
@@ -9,14 +9,14 @@ def search_by_city_district():
     res = []
     city = request.args.get('city')
     districts_list = request.args.getlist('district')
-    for district in districts_list:
-        ad = ws.search_last_ad(city, district)
+    if(districts_list != [] and city != None):
+        for district in districts_list:
+            ad = ws.search_last_ad(city, district)
+            res.append(json.loads(ad))
+    else:
+        ad = ws.search_last_ad()
         res.append(json.loads(ad))
     return jsonify(res)        
-
-@app.route("/search", methods=['GET'])
-def search():
-    return json.loads(ws.search_last_ad())
 
 @app.route("/<string:city>/districts")
 def find_all_districts(city):
